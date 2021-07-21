564484cde5d7c2889edcfc41908ae10d3a9578e9
NataLee38 <light3475house@gmail.com>
2021-07-21 19:06:01 +0300

    added ;

--- script.js
+++ script.js
@@ -4,37 +4,37 @@ let min = 38;
 let age = 16;
 
 if (day >= 1 && day <= 10) {
-    console.log("first decade")
+    console.log("first decade");
 } else if (day >=11 && day <=20) {
-    console.log("second decade")
+    console.log("second decade");
 } else if (day >=21 && day <= 31) {
-    console.log("third decade")
+    console.log("third decade");
 } else {
-    console.log("else")
+    console.log("else");
 }
 
 if (month >= 1 && month <= 2 || month === 12) {
-    console.log("winter")
+    console.log("winter");
 } else if (month >= 3 && month <=5) {
-    console.log("spring")
+    console.log("spring");
 } else if (month >= 6 && month <= 8) {
-    console.log("summer")
+    console.log("summer");
 } else if (month >= 9 && month <= 11) {
-    console.log("autumn")
+    console.log("autumn");
 } else {
-    console.log("else")
+    console.log("else");
 }
 
 if (min >= 0 && min <= 15) {
-    console.log("first quarter")
+    console.log("first quarter");
 } else if (min > 15 && min <= 30) {
-    console.log("second quarter")
+    console.log("second quarter");
 } else if (min > 30 && min <= 45) {
-    console.log("third quarter")
+    console.log("third quarter");
 } else if (min > 45 && min <=59) {
-    console.log("fourth quarter")
+    console.log("fourth quarter");
 } else {
-    console.log("else")
+    console.log("else");
 }
 
 age >= 18 ? console.log("adult") : console.log("underage");
